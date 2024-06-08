6ca8486e249d0e9863de8ca29ba72425ce32b9dc
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2024-06-08 19:11:58 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 import driver from "./driver";
 
 const app = new Hono();
 console.log("Server started?");
 app.use(logger());
+app.use(cors());
 app.get("/", async (c) => {
     let url;
     let param = c.req.queries("url");
